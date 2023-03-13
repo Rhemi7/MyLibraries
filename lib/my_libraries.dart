d225a445b9815c0296f8b0358d69bea490e238c9
aderemilekun <adelefatai@yahoo.com>
2023-03-13 22:52:02 +0100

    Added new libraries to export file

--- lib/my_libraries.dart
+++ lib/my_libraries.dart
@@ -2,5 +2,8 @@ export 'src/widgets/borderless_text_field.dart';
 export 'src/widgets/password_border_text_field.dart';
 export 'src/widgets/primary_custom_button.dart';
 export 'src/widgets/Animation/blinking_widget.dart';
+export 'src/libraries/string_extensions.dart';
+export 'src/libraries/resolution.dart';
+export 'src/libraries/margins.dart';
 
 
